ff797c653a22b312a43db804f3ddd073887edf3c
Spandan <royspandan1@gmail.com>
2023-06-15 15:45:47 +0530

    ray, vec3 and blue gradient

---
+++ src/include/math_utils/vec3.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <cmath>
+#include <iostream>
+
+class vec3
+{
+public:
+    double e[3];
+
+    vec3() : e{0, 0, 0} {}
+    vec3(double x, double y, double z) : e{x, y, z} {}
+
+    double x() const;
+    double y() const;
+    double z() const;
+
+    vec3 operator-() const;
+    double operator[](int i) const;
+    double &operator[](int i);
+
+    vec3 &operator+=(const vec3 &v);
+    vec3 &operator*=(const double t);
+    vec3 &operator/=(const double t);
+    double length() const;
+    double length_squared() const;
+};
+
+using point3 = vec3;
+using color = vec3;
+
+std::ostream &operator<<(std::ostream &out, const vec3 &v);
+vec3 operator+(const vec3 &u, const vec3 &v);
+vec3 operator-(const vec3 &u, const vec3 &v);
+vec3 operator*(const vec3 &u, const vec3 &v);
+vec3 operator*(double t, const vec3 &v);
+vec3 operator*(const vec3 &v, double t);
+vec3 operator/(vec3 v, double t);
+double dot(const vec3 &u, const vec3 &v);
+vec3 cross(const vec3 &u, const vec3 &v);
+vec3 unit_vector(vec3 v);
--- src/main.cpp
+++ src/main.cpp
@@ -1,61 +1,50 @@
 #include "pix/pix.hpp"
+#include "math_utils/vec3.hpp"
+#include "math_utils/ray.hpp"
 
-// Global variables
-int stripeOffset = 0;        // Tracks the offset of the stripes
-double lastUpdateTime = 0;   // Tracks the time of the last update
-double movementSpeed = 20.0; // Number of pixels to move per second
+const auto aspect_ratio = 16.0 / 9.0;
+const int image_width = 800;
+const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+auto viewport_height = 2.0;
+auto viewport_width = aspect_ratio * viewport_height;
+auto focal_length = 1.0;
+
+auto origin = point3(0, 0, 0);
+auto horizontal = vec3(viewport_width, 0, 0);
+auto vertical = vec3(0, viewport_height, 0);
+auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+
+color ray_color(const ray &r)
+{
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
 
 void renderCallback(Pix *pix)
 {
-    double currentTime = Pix::GetTime();
-    double deltaTime = currentTime - lastUpdateTime;
-
-    if (deltaTime < 1.0)
+    for (int j = pix->height - 1; j >= 0; --j)
     {
-        return;
-    }
-    lastUpdateTime = currentTime;
-
-    // Moving red, green, blue stripes 20 pixels wide, moving from right to left
-    for (int y = 0; y < pix->height; y++)
-    {
-        for (int x = 0; x < pix->width; x += 20)
+        for (int i = 0; i < pix->width; ++i)
         {
-            int stripeIndex = (x + stripeOffset) / 20;
+            auto u = double(i) / (pix->width - 1);
+            auto v = double(j) / (pix->height - 1);
+            ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+            color col = ray_color(r);
 
-            int r = 0;
-            int g = 0;
-            int b = 0;
+            int ir = static_cast<int>(255.999 * col.x());
+            int ig = static_cast<int>(255.999 * col.y());
+            int ib = static_cast<int>(255.999 * col.z());
 
-            switch (stripeIndex % 3)
-            {
-            case 0:
-                r = 255; // Red stripe
-                break;
-            case 1:
-                g = 255; // Green stripe
-                break;
-            case 2:
-                b = 255; // Blue stripe
-                break;
-            }
-
-            // Set the color values for each pixel in the stripe
-            for (int i = x; i < x + 20; i++)
-            {
-                pix->SetPixel(i, y, r, g, b);
-            }
+            pix->SetPixel(i, j, ir, ig, ib);
         }
     }
-
-    // Update stripe offset for the next frame
-    int pixelsToMove = (int)(movementSpeed * deltaTime);
-    stripeOffset = (stripeOffset + pixelsToMove) % pix->width;
 }
 
 int main()
 {
-    auto pix = Pix(800, 600, "Pixren");
+    auto pix = Pix(image_width, image_height, "Raytracer");
     pix.PixRun(renderCallback);
     return 0;
 }
